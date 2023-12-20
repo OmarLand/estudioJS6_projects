b556d28c076e8923fad9f48f5440ad5f0052cb63
OmarjLand <omar.land2010@gmail.com>
2023-12-20 23:06:23 +0100

    feat: Got to conect the APi of movies now-playing into my project

--- movies_project/js/app.js
+++ movies_project/js/app.js
@@ -1,4 +1,20 @@
-const hola = "Hola";
+document.addEventListener("DOMContentLoaded", async () => {
+    const movies = await getNewMovies();
+    console.log( movies );
+});
 
 
-console.log(hola);
+const getNewMovies = () => {
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNzNlOTA2MWRkYWNiNTJjMzYxMDgyYWI3NjcyZjQxZiIsInN1YiI6IjVlYTg2MmRmYjdmYmJkMDAxZDM5Njk2NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9DdxM-dSCMmQ29M_-BC25VuzPg6cel-EEVkXm-tOefY'
+        }
+    };
+      
+    return fetch('https://api.themoviedb.org/3/movie/now_playing?language=es-ES&page=1', options)
+        .then( response => response.json()  )
+        .then( response => response.results )
+        .catch( err => console.error(err)   );
+}
