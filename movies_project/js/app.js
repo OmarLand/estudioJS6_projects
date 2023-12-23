dd72ac316d681a2afd3744a41169b63ec60cba25
OmarjLand <omar.land2010@gmail.com>
2023-12-23 01:11:46 +0100

    feat: Added the carousel from boostrap and conected to the TMDB Api

--- movies_project/js/app.js
+++ movies_project/js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    const movies = await getNewMovies();
-    console.log( movies );
+    
+    renderNewsMovies();
+
 });
 
 
@@ -16,5 +17,43 @@ const getNewMovies = () => {
     return fetch('https://api.themoviedb.org/3/movie/now_playing?language=es-ES&page=1', options)
         .then( response => response.json()  )
         .then( response => response.results )
-        .catch( err => console.error(err)   );
+        .catch( err => console.error(err)  );
+}
+
+const renderNewsMovies = async() =>{
+    const newMovies = await getNewMovies();
+    //console.log( newMovies );
+
+    let html = '';
+
+    newMovies.forEach( (movie, index) => {
+
+        const { id, title, overview, backdrop_path } = movie;
+        const urlImage = `https://image.tmdb.org/t/p/w500${backdrop_path}`
+        const urlMovie = `../movie.html?id=${id}`
+        //console.log( index );
+        
+        html += `
+            <div class="carousel-item ${index === 0 ? "active" : null}" style="background-image: url('${urlImage}')">
+                <div class="carousel-caption">
+                    <h5>${title}</h5>
+                    <p>${overview}</p>
+                    <a href="${urlMovie}" class="btn btn-primary">Más información<a/>
+                </div>
+            </div>
+        `; 
+    });
+
+    html += `
+        <button class="carousel-control-prev" type="button" data-target="#carousel-news-movies" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-target="#carousel-news-movies" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </button>
+    `;
+
+    document.getElementsByClassName('list-news-movies')[0].innerHTML = html;
 }
