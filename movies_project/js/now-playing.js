3ed865e0be4cae4b80bdc2733575426ad1343ff2
OmarjLand <omar.land2010@gmail.com>
2024-01-13 02:04:59 +0100

    feat: Finished the Last Movies page with pagination

--- movies_project/js/now-playing.js
+++ movies_project/js/now-playing.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let { page } = getUrlVars();
     page == undefined ? page = 1 : null; 
     renderNewsMovies( page );
+    renderControls( page );
 });
 
 const getUrlVars = () => {
@@ -48,4 +49,80 @@ const renderNewsMovies = async ( page ) => {
     });
 
     document.getElementsByClassName("list-cards")[0].innerHTML = html;
+}
+
+const renderControls = ( page ) => {
+    const baseUrlPage = "../now-playing.html?page=";
+    const pageNumber = parseInt(page);
+    // console.log( pageNumber + 1 ); 
+    const previous = pageNumber - 1;
+    const next = pageNumber + 1;
+
+    let html = "";
+
+    if( page == 1 ){
+        html += `
+        
+            <ul class="pagination justify-content-center">
+                
+                <li class="page-item disabled">
+                    <a href="#" class="page-link">
+                        <i class="fas fa-chevron-left"></i>
+                    </a>
+                </li>
+
+                <li class="page-item active">
+                    <a href="${baseUrlPage + "1"}" class="page-link">1</a>
+                </li>
+                <li class="page-item">
+                    <a href="${baseUrlPage + "2"}" class="page-link">2</a>
+                </li>
+                <li class="page-item">
+                    <a href="${baseUrlPage + "3"}" class="page-link">3</a>
+                </li>
+
+                <li class="page-item">
+                    <a href="${baseUrlPage + "2"}" class="page-link">
+                        <i class="fas fa-chevron-right"></i>
+                    </a>
+                </li>
+
+
+            </ul>
+
+        `;
+    } else {
+        html += `
+            
+            <ul class="pagination justify-content-center">
+                
+                <li class="page-item">
+                    <a href="${baseUrlPage - previous}" class="page-link">
+                        <i class="fas fa-chevron-left"></i>
+                    </a>
+                </li>
+
+                <li class="page-item">
+                    <a href="${baseUrlPage + previous}" class="page-link">${previous}</a>
+                </li>
+                <li class="page-item active">
+                    <a href="${baseUrlPage + page}" class="page-link">${page}</a>
+                </li>
+                <li class="page-item">
+                    <a href="${baseUrlPage + next}" class="page-link">${next}</a>
+                </li>
+
+                <li class="page-item">
+                    <a href="${baseUrlPage + next}" class="page-link">
+                        <i class="fas fa-chevron-right"></i>
+                    </a>
+                </li>
+
+            </ul>
+
+        `;
+    }
+
+    document.getElementsByClassName("navigation")[0].innerHTML = html;
+
 }
