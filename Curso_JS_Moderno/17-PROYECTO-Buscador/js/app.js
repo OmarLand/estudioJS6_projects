fb3c499ca53c298c2d6ce5438e61e32dde49d5b0
OmarjLand <omar.land2010@gmail.com>
2024-05-27 22:26:55 +0200

    Afinando detalles para la funcion de busqueda de carros

--- Curso_JS_Moderno/17-PROYECTO-Buscador/js/app.js
+++ Curso_JS_Moderno/17-PROYECTO-Buscador/js/app.js
@@ -1,11 +1,26 @@
 //Variables
-const resultado = document.querySelector('#resultado');
-const year = document.querySelector('#year')
+const marca       = document.querySelector('#marca')
+const year        = document.querySelector('#year')
+const minimo      = document.querySelector('#minimo')
+const maximo      = document.querySelector('#maximo')
+const puertas     = document.querySelector('#puertas')
+const transmision = document.querySelector('#transmision')
+const color       = document.querySelector('#color')
+const resultado   = document.querySelector('#resultado');
 
 const max = new Date().getFullYear();
 const min = max - 10;
 
-// console.log(max, min);
+// Generar un objeto con la búsqueda
+const datosBusqueda = {
+    marca       : '',
+    year        : '',
+    minimo      : '',
+    maximo      : '',
+    puertas     : '',
+    transmision : '',
+    color       : '',
+}
 
 //Eventos
 document.addEventListener('DOMContentLoaded', () => {
@@ -15,8 +30,36 @@ document.addEventListener('DOMContentLoaded', () => {
     // Llena las opciones de años
     llenarSelect();
 
-})
+});
 
+// Event listeners para los select de la busqueda
+marca.addEventListener('change', (e) => {
+    datosBusqueda.marca = e.target.value;
+});
+
+year.addEventListener('change', (e) => {
+    datosBusqueda.year = e.target.value;
+});
+
+minimo.addEventListener('change', (e) => {
+    datosBusqueda.minimo = e.target.value;
+});
+
+maximo.addEventListener('change', (e) => {
+    datosBusqueda.maximo = e.target.value;
+});
+
+puertas.addEventListener('change', (e) => {
+    datosBusqueda.puertas = e.target.value;
+});
+
+transmision.addEventListener('change', (e) => {
+    datosBusqueda.transmision = e.target.value;
+});
+
+color.addEventListener('change', (e) => {
+    datosBusqueda.transmision = e.target.value;
+})
 
 //Funciones
 function mostrarAutos(){
