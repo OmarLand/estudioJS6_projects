c919685656b6b0124a3d70231d914111e68d2d6b
OmarjLand <omar.land2010@gmail.com>
2024-05-27 23:05:43 +0200

    Añadimos filtro para la busqueda de nro puertas - transmision y color de coche

--- Curso_JS_Moderno/17-PROYECTO-Buscador/js/app.js
+++ Curso_JS_Moderno/17-PROYECTO-Buscador/js/app.js
@@ -47,22 +47,32 @@ year.addEventListener('change', (e) => {
 
 minimo.addEventListener('change', (e) => {
     datosBusqueda.minimo = e.target.value;
+
+    filtrarAuto();
 });
 
 maximo.addEventListener('change', (e) => {
     datosBusqueda.maximo = e.target.value;
+    
+    filtrarAuto();
 });
 
 puertas.addEventListener('change', (e) => {
-    datosBusqueda.puertas = e.target.value;
+    datosBusqueda.puertas = parseInt( e.target.value );
+
+    filtrarAuto();
 });
 
 transmision.addEventListener('change', (e) => {
     datosBusqueda.transmision = e.target.value;
+
+    filtrarAuto();
 });
 
 color.addEventListener('change', (e) => {
-    datosBusqueda.transmision = e.target.value;
+    datosBusqueda.color = e.target.value;
+
+    filtrarAuto();
 })
 
 //Funciones
@@ -105,7 +115,7 @@ function llenarSelect(){
 
 // Funcion que filtra en base a  la busqueda
 function filtrarAuto(){
-    const resultado = autos.filter( filtraMarca ).filter( filtrarYear );
+    const resultado = autos.filter( filtraMarca ).filter( filtrarYear ).filter( filtrarMinimo ).filter( filtrarMaximo ).filter( filtrarPuertas ).filter(filtrarTransmision).filter( filtrarColor );
     // console.log( resultado );
 
     mostrarAutos(resultado); 
@@ -123,6 +133,46 @@ function filtrarYear( auto ){
     const { year } = datosBusqueda;
     if( year ){
         return auto.year === year;
+    }
+    return auto;
+}
+
+function filtrarMinimo( auto ){
+    const { minimo } = datosBusqueda;
+    if( minimo ){
+        return auto.precio >= minimo ;
+    }
+    return auto;
+}
+
+function filtrarMaximo( auto ){
+    const { maximo } = datosBusqueda;
+    if( maximo ){
+        return auto.precio <= maximo ;
+    }
+    return auto;
+}
+
+function filtrarPuertas( auto ){
+    const { puertas } = datosBusqueda;
+    if( puertas ){
+        return auto.puertas === puertas ;
+    }
+    return auto;
+}
+
+function filtrarTransmision( auto ){
+    const { transmision } = datosBusqueda;
+    if( transmision ){
+        return auto.transmision === transmision ;
+    }
+    return auto;
+}
+
+function filtrarColor( auto ){
+    const { color } = datosBusqueda;
+    if( color ){
+        return auto.color === color ;
     }
     return auto;
 }
