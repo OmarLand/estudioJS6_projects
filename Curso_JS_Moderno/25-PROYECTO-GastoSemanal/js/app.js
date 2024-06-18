25f3773410fb7e03821b5a9a869478bff695c9cd
OmarjLand <omar.land2010@gmail.com>
2024-06-18 22:41:17 +0200

    feat: validando valores del formulario de gastos

--- Curso_JS_Moderno/25-PROYECTO-GastoSemanal/js/app.js
+++ Curso_JS_Moderno/25-PROYECTO-GastoSemanal/js/app.js
@@ -7,6 +7,8 @@ const gastoListado = document.querySelector('#gastos ul');
 eventListeners();
 function eventListeners(){
     document.addEventListener( 'DOMContentLoaded', preguntarPresupuesto );
+
+    formulario.addEventListener( 'submit', agregarGasto );
 }
 
 
@@ -21,12 +23,44 @@ class Presupuesto {
 }
 
 class UI{
+    insertarPresupuesto( cantidad ){
+        // Extraigo los valores
+        const { presupuesto, restante } = cantidad;
+
+        // Los añado al HTML
+        document.querySelector('#total').textContent = presupuesto;
+        document.querySelector('#restante').textContent = restante;
+    }
+
+    imprimirAlerta(mensaje, tipo){
+        // Crear el DIV 
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert');
+
+        if(tipo === 'error'){
+            divMensaje.classList.add('alert-danger');
+        } else {
+            divMensaje.classList.add('alert-success');
+        }
+
+        // Crear mensaje de error
+        divMensaje.textContent = mensaje;
+
+        // insertar en el HTML
+        document.querySelector('.primario').insertBefore(divMensaje, formulario);
+
+        // Quitar mensaje de error
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
+
+    }
 
 }
 
 
 // Instanciar
-const ui = newUI();
+const ui = new UI();
 let presupuesto;
 
 
@@ -45,4 +79,26 @@ function preguntarPresupuesto(){
     // Presupuesto válido
     presupuesto = new Presupuesto(presupuestoUsuario);
     console.log( presupuesto );
-} 
+
+    ui.insertarPresupuesto( presupuesto )
+}
+
+// Añade gastos
+function agregarGasto(e){
+    e.preventDefault();
+
+    // Leer los datos del formulario
+    const nombre   = document.querySelector('#gasto').value;
+    const cantidad = document.querySelector('#cantidad').value;
+
+    // Validamos campos
+    if( nombre === '' || cantidad === '' ){
+        ui.imprimirAlerta('Ambos campos son obligatorios', 'error');
+        return;
+    } else if( cantidad <= 0 || isNaN(cantidad) ) {
+        ui.imprimirAlerta('Cantidad no valida', 'error');
+        return;
+    }
+
+    console.log('Agregando el gasto...');
+}
