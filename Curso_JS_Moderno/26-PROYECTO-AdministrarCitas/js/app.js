a72e46ed8d7cfc68f26011902f3f54f391af7662
OmarjLand <omar.land2010@gmail.com>
2024-08-14 18:56:54 +0200

    Avanzando con el proyecto de citas medicas

--- Curso_JS_Moderno/26-PROYECTO-AdministrarCitas/js/app.js
+++ Curso_JS_Moderno/26-PROYECTO-AdministrarCitas/js/app.js
@@ -34,11 +34,54 @@ function submitCita(e){
     e.preventDefault();
 
     if( Object.values(citaObj).some( valor => valor.trim() === '' )){
-        console.log( 'Todos los campos son obligatorios' );
+        // console.log( 'Todos los campos son obligatorios' );
+        const notificacion = new Notificacion({
+            texto : 'Todos los campos son obligatorios',
+            tipo  : 'error' 
+        }) 
+
+        // console.log('>>>', notificacion );
+        notificacion.mostrar();
         return;
+        
     }
 
     console.log('Despues del If...');
-    
- 
+}
+
+class Notificacion {
+
+    constructor({texto, tipo}) {
+        this.texto = texto;
+        this.tipo  = tipo;
+
+        // this.mostrar();
+    }
+
+    mostrar(){
+        // Crear la notificacion
+        const alerta = document.createElement('div');
+        alerta.classList.add('text-center','w-full','p-3','text-white','my-5','alert','uppercase','font-bold','text-sm');
+
+        // Eliminar alertas duplicadas
+        const alertaPrevia = document.querySelector('.alert');
+        
+       alertaPrevia?.remove()
+
+        // Si es de tipo error, agrega una clase 
+        this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500');
+
+        // Mensaje de error
+        alerta.textContent = this.texto;
+
+        // Insertar al DOM
+        formulario.parentElement.insertBefore(alerta, formulario);
+
+        // Eliminar despues de 5 segundos el alerta
+        setTimeout(() => {
+            alerta.remove();
+        }, 5000);
+        
+    }
+
 }
