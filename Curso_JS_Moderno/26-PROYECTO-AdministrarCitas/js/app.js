4687426b24c8989e2fc7fdb85374ef720cf1ae3f
OmarjLand <omar.land2010@gmail.com>
2024-08-20 21:08:27 +0200

    Añadimos control al botón editar

--- Curso_JS_Moderno/26-PROYECTO-AdministrarCitas/js/app.js
+++ Curso_JS_Moderno/26-PROYECTO-AdministrarCitas/js/app.js
@@ -6,7 +6,13 @@ const fechaInput       = document.querySelector('#fecha');
 const sintomasInput    = document.querySelector('#sintomas');
 
 const formulario      = document.querySelector('#formulario-cita');
-const contenedorCitas = document.querySelector('#citas')
+const contenedorCitas = document.querySelector('#citas');
+
+const btnEditar       = document.querySelector('.btn-editar');
+
+// btnEditar?.addEventListener('click', () => {
+//     alert('Diste click en el btn-Editar');
+// })
 
 // Eventos
 pacienteInput.addEventListener('change', datosCita)
@@ -109,8 +115,11 @@ class AdminCitas {
 
             // Botones Eliminar y editar
             const btnEditar = document.createElement('button');
-            btnEditar.classList.add('py-2', 'px-10', 'bg-indigo-600', 'hover:bg-indigo-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
+            btnEditar.classList.add('py-2', 'px-10', 'bg-indigo-600', 'hover:bg-indigo-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2','btn-editar');
             btnEditar.innerHTML = 'Editar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
+            const clone = structuredClone( cita )
+            btnEditar.onclick = () => cargarEdicion( clone );
+
 
             const btnEliminar = document.createElement('button');
             btnEliminar.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
@@ -175,4 +184,10 @@ function reiniciarObjetoCita(){
     citaObj.email       = '';
     citaObj.fecha       = '';
     citaObj.sintomas    = '';
+}
+
+function cargarEdicion( cita ){
+    // alert('Hasta dado click...');
+    console.log( cita );
+    
 }
