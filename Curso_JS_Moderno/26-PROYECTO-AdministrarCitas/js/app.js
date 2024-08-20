106ef120d0ea5dcb3f84b9a7fb4e29b5bdafb4ba
OmarjLand <omar.land2010@gmail.com>
2024-08-20 20:47:56 +0200

    Añado botones Editar y eliminar en el contenido del paciente

--- Curso_JS_Moderno/26-PROYECTO-AdministrarCitas/js/app.js
+++ Curso_JS_Moderno/26-PROYECTO-AdministrarCitas/js/app.js
@@ -107,12 +107,28 @@ class AdminCitas {
             sintomas.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case')
             sintomas.innerHTML = `<span class="font-bold uppercase">Síntomas: </span> ${cita.sintomas}`;
 
+            // Botones Eliminar y editar
+            const btnEditar = document.createElement('button');
+            btnEditar.classList.add('py-2', 'px-10', 'bg-indigo-600', 'hover:bg-indigo-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
+            btnEditar.innerHTML = 'Editar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
+
+            const btnEliminar = document.createElement('button');
+            btnEliminar.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
+            btnEliminar.innerHTML = 'Eliminar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+
+            const contenedoresBotones = document.createElement('div');
+            contenedoresBotones.classList.add('flex','justify-between', 'mt-10');
+
+            contenedoresBotones.appendChild(btnEditar);
+            contenedoresBotones.appendChild(btnEliminar);
+
             // Agregar al HTML
             divCita.appendChild(paciente);
             divCita.appendChild(propietario);
             divCita.appendChild(email);
             divCita.appendChild(fecha);
             divCita.appendChild(sintomas);
+            divCita.appendChild(contenedoresBotones);
             contenedorCitas.appendChild(divCita);
         })
     }
@@ -135,12 +151,12 @@ function submitCita(e){
             texto : 'Todos los campos son obligatorios',
             tipo  : 'error' 
         }) 
-        
+
         return;
         
     }
 
-    citas.agregar(citaObj);
+    citas.agregar({...citaObj});
     formulario.reset();
     reiniciarObjetoCita();
     
