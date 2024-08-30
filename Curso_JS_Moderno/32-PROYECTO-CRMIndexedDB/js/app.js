b4d9414cda93099d7846507f006560053965ae27
OmarjLand <omar.land2010@gmail.com>
2024-08-30 22:44:32 +0200

    Proecto terminado de registro de clientes CRM

--- Curso_JS_Moderno/32-PROYECTO-CRMIndexedDB/js/app.js
+++ Curso_JS_Moderno/32-PROYECTO-CRMIndexedDB/js/app.js
@@ -1,6 +1,8 @@
-let DB;
 
 (function(){
+
+    let DB;
+    const listadoClientes = document.querySelector('#listado-clientes');
 
     document.addEventListener('DOMContentLoaded', () =>{
 
@@ -10,7 +12,36 @@ let DB;
             obtenerCliente();
         }
 
+        listadoClientes.addEventListener('click', eliminarRegistro)
+
     });
+
+    function eliminarRegistro(e){
+        //console.log(e.target.classList.contains('eliminar'));
+        if( e.target.classList.contains('eliminar') ){
+            //console.log('Diste click en Eliminar...');
+            const idEliminar = Number(e.target.dataset.cliente);
+            
+            const confirmar = confirm('¿Realmente desea eliminar el registro?');
+            if(confirmar){
+                const transaction = DB.transaction(['crm'], 'readwrite');
+                const objectStore = transaction.objectStore('crm');
+
+                objectStore.delete(idEliminar);
+
+                transaction.oncomplete = function(){
+                    console.log('Registro eliminado correctamente');
+                    e.target.parentElement.parentElement.remove();
+                }
+
+                transaction.onerror = function(){
+                    console.log('Error al elminar el registro','error')
+                }
+            }
+            
+            
+        }
+    }
 
     // Crea la base de datos
     function crearDB() {
@@ -57,7 +88,6 @@ let DB;
                     console.log( cursor.value );
                     const{ nombre, empresa, email, telefono, id } = cursor.value;
 
-                    const listadoClientes = document.querySelector('#listado-clientes');
                     listadoClientes.innerHTML += ` 
                     <tr>
                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
@@ -72,7 +102,7 @@ let DB;
                         </td>
                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
                             <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                            <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                            <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
                         </td>
                     </tr>
                     `;
