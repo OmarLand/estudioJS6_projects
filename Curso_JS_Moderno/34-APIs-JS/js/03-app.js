77d242b0d1ae51f1cab6033012bc0689a6433ecc
OmarjLand <omar.land2010@gmail.com>
2024-08-31 00:22:51 +0200

    Comprendiendo el API que comprueba si estamos o no online

---
+++ Curso_JS_Moderno/34-APIs-JS/js/03-app.js
@@ -0,0 +1,12 @@
+window.addEventListener('online', actualizarEstado);
+window.addEventListener('offline', actualizarEstado);
+
+function actualizarEstado(){
+    if( navigator.onLine ){
+        console.log('Si estás conectado');
+    } else {
+        console.log('No estas conectado...');
+        
+    }
+
+}
