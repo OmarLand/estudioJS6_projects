205e7f9d6f72b2141ec299a1db6aaf8dde48d0ca
OmarjLand <omar.land2010@gmail.com>
2024-08-30 23:09:41 +0200

    Ejemplo del Callback Hell

--- Curso_JS_Moderno/33-Promises/js/02-app.js
+++ Curso_JS_Moderno/33-Promises/js/02-app.js
@@ -1,16 +1,29 @@
 const paises = [];
 
-function nuevoPais(){
-
+function nuevoPais(pais, callback){
+    paises.push(pais);
+    console.log(`Añadiendo *${pais}*`);
+    callback();
+    
 }
 
 function mostrarPaises(){
-
+    console.log('>', paises);
+    
 }
 
 
 function inicialCallBackHell(){
+    setTimeout(() => {
+        nuevoPais('Alemania', mostrarPaises);
 
+        setTimeout(() => {
+            nuevoPais('España', mostrarPaises)
+            setTimeout(() => {
+                nuevoPais('Venezuela', mostrarPaises)
+            }, 3000);
+        }, 3000);
+    }, 3000);
 }
 
 
