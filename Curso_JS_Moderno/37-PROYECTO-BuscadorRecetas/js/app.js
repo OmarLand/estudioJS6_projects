93362d706d10d0f3fdd2136b6e95507cfd012068
OmarjLand <omar.land2010@gmail.com>
2024-09-05 23:15:30 +0200

    Mostramos en el XML las categorias de las recetas

--- Curso_JS_Moderno/37-PROYECTO-BuscadorRecetas/js/app.js
+++ Curso_JS_Moderno/37-PROYECTO-BuscadorRecetas/js/app.js
@@ -1,18 +1,29 @@
 function iniciarApp(){
 
+    const selectCategorias = document.querySelector('#categorias');
+
     obtenerCategorias();
 
     function obtenerCategorias(){
         const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
         fetch(url)
-            .then( res => {
-                return res.json();
-            })
-            .then( res => {
-                console.log( res );
-                
-            })
-        
+            .then( res =>  res.json() )
+            .then( res =>  mostrarCategorias( res.categories ) )
+    }
+
+    function mostrarCategorias(categorias = []){
+        categorias.forEach( categoria =>{
+
+            const { strCategory } = categoria; 
+
+            // console.log( categoria );
+            const option = document.createElement('option');
+            option.value = strCategory;
+            option.textContent = categoria.strCategory;
+            console.log( option );
+            selectCategorias.appendChild(option)
+            
+        })
     }
 
 }
