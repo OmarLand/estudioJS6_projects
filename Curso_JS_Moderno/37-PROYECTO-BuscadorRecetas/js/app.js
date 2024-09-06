1ba9b7448586b34b801bce6d57f6a64b8efbc574
OmarjLand <omar.land2010@gmail.com>
2024-09-06 23:19:07 +0200

    Mostrando elementos en el HTMLç

--- Curso_JS_Moderno/37-PROYECTO-BuscadorRecetas/js/app.js
+++ Curso_JS_Moderno/37-PROYECTO-BuscadorRecetas/js/app.js
@@ -1,7 +1,9 @@
 function iniciarApp(){
 
     const selectCategorias = document.querySelector('#categorias');
-    selectCategorias.addEventListener('change', seleccionarCategoria)
+    selectCategorias.addEventListener('change', seleccionarCategoria);
+
+    const resultado = document.querySelector('#resultado');
 
     obtenerCategorias();
 
@@ -32,11 +34,52 @@ function iniciarApp(){
         const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`
         fetch(url)
             .then(res => res.json() )
-            .then (res => console.log( res ) )
+            .then (res => mostrarRecetas( res.meals ) )
+    }
+
+    function mostrarRecetas( recetas = [] ){
+        // console.log( recetas );
+        // Iterar en los resultados
+
         
-        
-        
-        
+        recetas.forEach( receta => {
+            const { idMeal, strMeal, strMealThumb } = receta;
+
+            const recetaContenedor = document.createElement('div');
+            recetaContenedor.classList.add('col-md-4');
+
+            const recetaCard = document.createElement('div');
+            recetaCard.classList.add('card','mb-4')
+
+            const recetaImagen = document.createElement('img');
+            recetaImagen.classList.add('card-img-top');
+            recetaImagen.alt =  `Imagen de la Receta ${strMeal}`;
+            recetaImagen.src =  strMealThumb;
+
+            const recetaCardBody = document.createElement('div');
+            recetaCardBody.classList.add('card-body');
+
+            const recetaHeading = document.createElement('h3');
+            recetaHeading.classList.add('card-title','mb-3');
+            recetaHeading.textContent = strMeal;
+
+            const recetaButton = document.createElement('button');
+            recetaButton.classList.add('btn', 'btn-danger','w-100');
+            recetaButton.textContent = 'Ver Receta';
+
+            // Inyectar en el código el HTML
+            recetaCardBody.appendChild(recetaHeading);
+            recetaCardBody.appendChild(recetaButton);
+
+            recetaCard.appendChild(recetaImagen);
+            recetaCard.appendChild(recetaCardBody);
+
+            recetaContenedor.appendChild(recetaCard);
+
+            resultado.appendChild(recetaContenedor);
+            
+        })
+
     }
 
 }
