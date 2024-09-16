76f6d98fcc241749a09f764077382b7aad2e68a0
OmarjLand <omar.land2010@gmail.com>
2024-09-16 21:30:26 +0200

    feat: Corregido error de Toast que no mostraba al eliminar favorito

--- Curso_JS_Moderno/37-PROYECTO-BuscadorRecetas/js/app.js
+++ Curso_JS_Moderno/37-PROYECTO-BuscadorRecetas/js/app.js
@@ -196,7 +196,7 @@ function iniciarApp(){
     function eliminarFavorito(id){
         const favoritos = JSON.parse(localStorage.getItem('favoritos') ) ?? [];
         const nuevosFavoritos = favoritos.filter( favorito => favorito.id !== id );
-        localStorage.setItem('favoritos', JSOIN.stringify(nuevosFavoritos) )
+        localStorage.setItem('favoritos', JSON.stringify(nuevosFavoritos) )
     }
     
     function existeStorage( id ){
