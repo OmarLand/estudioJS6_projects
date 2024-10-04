209bebbc90ab71220542aed7ab57aa544e42c6fb
OmarjLand <omar.land2010@gmail.com>
2024-10-04 13:35:57 +0200

    Comenzando ejercicio sistema de restaurtant con propinas REST

---
+++ Curso_JS_Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -0,0 +1,14 @@
+const cliente = {
+    mesa   : '',
+    hora   : '',
+    pedido : [],
+};
+
+const btnGuardarCliente = document.querySelector('#guardar-cliente');
+
+btnGuardarCliente.addEventListener('click', guardarCliente);
+
+function guardarCliente(){
+    console.log('La función,..,..');
+    
+}
