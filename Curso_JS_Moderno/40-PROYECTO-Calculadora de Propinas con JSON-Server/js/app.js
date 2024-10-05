6b34b31a30610fc6141e52fc2d76f0fcb5281bb5
OmarjLand <omar.land2010@gmail.com>
2024-10-05 18:17:21 +0200

    Añadiendo validación al formulario

--- Curso_JS_Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ Curso_JS_Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -16,10 +16,26 @@ function guardarCliente(){
     const camposVacios = [ mesa, hora ].some(campo => campo === '');
 
     if( camposVacios ){
-        console.log('Si hay al menos un campo vacio');
-    } else {
-        console.log('Tdos los campos están correctos');
-        
-    }
+
+        // Verificar si existe ya un alerta
+        const existeAlerta = document.querySelector('.invalid-feedback');
+
+        if(!existeAlerta){
+            const alerta = document.createElement('div');
+            alerta.classList.add('invalid-feedback', 'd-block','text-center');
+            alerta.textContent = 'Todos los campos son obligatorio';
+            document.querySelector('.modal-body form').appendChild(alerta);
+
+            // Eliminamos alerta
+            setTimeout(() => {
+                alerta.remove();
+            }, 3000);
+        }
+
+        return;
+
+    } 
+        console.log('Todos los campos están correctos');
+    
     
 }
