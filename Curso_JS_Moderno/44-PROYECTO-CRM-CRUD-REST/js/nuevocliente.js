c0a2ddca3d1a5a970bada76ea6e4c3a3b21e1444
OmarjLand <omar.land2010@gmail.com>
2024-11-29 23:36:17 +0100

    Validación de nuevos clientes & mostrar mjs si no pasa validacion del form

---
+++ Curso_JS_Moderno/44-PROYECTO-CRM-CRUD-REST/js/nuevocliente.js
@@ -0,0 +1,40 @@
+import { mostrarAlerta } from "./funciones.js";
+
+(function(){
+
+    const formulario = document.querySelector('#formulario');
+    formulario.addEventListener('submit', validarCliente);
+
+    function validarCliente( e ){
+        e.preventDefault();
+
+        const nombre   = document.querySelector('#nombre').value;
+        const email    = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa  = document.querySelector('#empresa').value;
+
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa
+        }
+
+        // console.log( !Object.values(cliente).every( input => input !== '' ) );
+        
+        if( validar(cliente) ){
+            // Mostramos mensaje
+            mostrarAlerta('Todos los campos son obligatorios');
+            return;
+        }
+
+        console.log('Si pasó la validación...');
+        
+        // Validación de los campos del formulario que todos estén cubiertos
+        function validar(obj){
+            return !Object.values(cliente).every( input => input !== '' );
+        }
+
+    }
+
+})();
