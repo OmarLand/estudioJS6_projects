e6f7fdb6748126520fac4d3b76c75f492d8ea1a6
OmarjLand <omar.land2010@gmail.com>
2024-12-01 19:21:15 +0100

    Elminando un cliente de la base de datos

--- Curso_JS_Moderno/44-PROYECTO-CRM-CRUD-REST/js/API.js
+++ Curso_JS_Moderno/44-PROYECTO-CRM-CRUD-REST/js/API.js
@@ -1,5 +1,6 @@
 const url = 'http://localhost:3000/clientes';
 
+// Cuando se crea nuevo cliente
 export const nuevoCliente = async ( cliente ) => {
     
     try {
@@ -13,8 +14,34 @@ export const nuevoCliente = async ( cliente ) => {
         window.location.href = 'index.html';
    
     } catch (error) {
+        console.log( error );   
+    }
+    
+}
+
+// Obtiene todos los clientes
+export const obtenerClientes = async() => {
+    try {
+        const resultado = await fetch(url) ;
+        const clientes = await resultado.json();
+        return clientes;
+
+    } catch (error) {
         console.log( error );
         
     }
-    
+}
+
+// Elimina un cliente...
+export const eliminarCliente = async (id) => {
+
+    try {
+        await fetch(`${url}/${id}`, {
+            method : 'DELETE'
+        })
+    } catch (error) {
+        console.log( error );
+        
+    }
+
 }
