a97a79413ae9eb1785a423134d8da562a7860bd8
OmarjLand <omar.land2010@gmail.com>
2024-12-14 14:42:26 +0100

    Cacheando datos de la pagina

---
+++ Curso_JS_Moderno/47-ServiceWorkers-PWA/sw.js
@@ -0,0 +1,33 @@
+const nombreCache = 'apv-v1';
+const archivos = [
+    '/',
+    '/index.html',
+    '/css/bootstrap.css',
+    '/css/styles.css',
+    '/js/apv.js'
+];
+
+// Cuando se instala el ServiceWorker
+self.addEventListener('install', e => {
+    console.log('Instalado el Service Worker');
+    //console.log( e );
+    e.waitUntil(
+        caches.open(nombreCache)
+            .then( cache => {
+                console.log('Cacheando...');
+                cache.addAll( archivos )
+                
+            })
+    )
+});
+
+// Activar el Service Worker
+self.addEventListener('activate', e => {
+    console.log( 'Service Worker Activado...!!! xD' );
+    console.log('>>> ',e);  
+});
+
+// EVento fetch para descargar archivos estaticos:
+self.addEventListener('fetch', e => {
+    console.log( 'Fetch...', e );
+})
