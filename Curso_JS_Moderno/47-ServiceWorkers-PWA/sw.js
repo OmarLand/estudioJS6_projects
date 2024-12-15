61dec08f972ddc6dd7d0c6c19763a2ddb73f344c
OmarjLand <omar.land2010@gmail.com>
2024-12-15 16:51:45 +0100

    Añadiendo pagina de error.html

--- Curso_JS_Moderno/47-ServiceWorkers-PWA/sw.js
+++ Curso_JS_Moderno/47-ServiceWorkers-PWA/sw.js
@@ -2,6 +2,7 @@ const nombreCache = 'apv-v1';
 const archivos = [
     '/',
     '/index.html',
+    '/error.html',
     '/css/bootstrap.css',
     '/css/styles.css',
     '/js/apv.js'
@@ -30,4 +31,12 @@ self.addEventListener('activate', e => {
 // EVento fetch para descargar archivos estaticos:
 self.addEventListener('fetch', e => {
     console.log( 'Fetch...', e );
+
+    e.respondWith(
+        caches.match(e.request)
+            .then( respuestaCache => {
+                return respuestaCache
+            })
+            .catch( () => caches.match('/error.html') )
+    )
 })
