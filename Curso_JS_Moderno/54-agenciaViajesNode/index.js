2a5c011c0640fc06791bdfbd5a6917ec2c340d43
OmarjLand <omar.land2010@gmail.com>
2025-01-26 21:58:28 +0100

    Terminado el Curso - Está incompleto de informacion

--- Curso_JS_Moderno/54-agenciaViajesNode/index.js
+++ Curso_JS_Moderno/54-agenciaViajesNode/index.js
@@ -10,6 +10,16 @@ const port = process.env.PORT || 4000;
 // Habilitar PUG
 app.set('view engine', 'pug');
 
+// Obtener el año actual
+app.use( (req, res, next) =>{
+    const year = new Date();
+
+    res.locals.actualYear  = year.getFullYear();
+    res.locals.nombresitio = 'Agencia de Viajes';
+    return next();
+    
+})
+
 // Definir la carpeta publica
 app.use( express.static('public') );
 
--- Curso_JS_Moderno/54-agenciaViajesNode/routes/index.js
+++ Curso_JS_Moderno/54-agenciaViajesNode/routes/index.js
@@ -3,12 +3,27 @@ import express from "express";
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('inicio')
-})
+    res.render('inicio', {
+        pagina: 'Inicio'
+    });
+});
 
 router.get('/nosotros', (req, res)=> {
     res.render('nosotros', {
+        pagina: 'Nosotros'
     });
-})
+});
+
+router.get('/viajes', (req, res)=> {
+    res.render('viajes', {
+        pagina: 'Viajes'
+    });
+});
+
+router.get('/testimoniales', (req, res)=> {
+    res.render('testimoniales', {
+        pagina: 'Testimoniales'
+    });
+});
 
 export default router;
